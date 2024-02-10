fc3097eaa834986dd33e4531ab848972adee41c3
Katherine-22 <kuznetsova.kateryn@gmail.com>
2024-02-10 19:57:21 +0200

    Updated main.js

---
+++ dist/js/main.js
@@ -0,0 +1,47 @@
+// menu nav oprn-close toggle button
+const menuBtn = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn__burger");
+
+const menuNav = document.querySelector(".menu-nav");
+const menuList = document.querySelector(".menu-nav__list");
+const signupBtn = document.querySelector(".signup__btn");
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (showMenu === false) {
+    hamburger.classList.add("open");
+    document.body.style.overflowY = "hidden";
+    signupBtn.classList.add("open");
+    menuNav.classList.add("open");
+    menuList.classList.add("open");
+
+    showMenu = true;
+  } else {
+    hamburger.classList.remove("open");
+    document.body.style.overflowY = "auto";
+    signupBtn.classList.remove("open");
+    menuNav.classList.remove("open");
+    menuList.classList.remove("open");
+
+    showMenu = false;
+  }
+}
+
+// accordion main page
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
+);
+accordionItemHeaders.forEach((header) => {
+  header.addEventListener("click", () => {
+    header.classList.toggle("active");
+    const itemBody = header.nextElementSibling;
+    if (header.classList.contains("active")) {
+      itemBody.style.maxHeight = itemBody.scrollHeight + "px";
+    } else {
+      itemBody.style.maxHeight = 0;
+    }
+  });
+});
